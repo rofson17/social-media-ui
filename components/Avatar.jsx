caa9110d9f5629663b9c668692308b934cf23c89
Rofson Jame Abu Siam <abu.siam610@gmail.com>
2023-02-02 15:37:31 +0600

    add navigation

---
+++ components/Avatar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Avatar = () => {
+    return (
+        <div className="w-12 h-12 shadow-md shadow-gray-400 rounded-full overflow-hidden">
+            <img src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YnVzaW5lc3MlMjBtYW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="user image" />
+        </div>
+    )
+}
+
+export default Avatar
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
+import Card from "@/components/Card"
+import Navigation from "@/components/Navigation"
+import PostFormCard from "@/components/PostFormCard"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="flex mt-4  max-w-4xl mx-auto gap-6 ">
+
+      <div className="w-1/3">
+        <Navigation />
+      </div>
+
+
+      <div className="grow">
+        <PostFormCard />
+        <Card>first post </Card>
+      </div>
+
+    </div>
+
   )
 }
 
